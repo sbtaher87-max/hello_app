9f991be9f18a9366432c42ccad630ac2079615e2
sbtaher87-max <sbtaher87@gmail.com>
2026-04-21 11:17:59 +0530

    added only uc7

---
+++ src/main/java/org/example/uc7.java
@@ -0,0 +1,12 @@
+package org.example;
+public class uc7 {
+    public static void main(String[] args) {
+        String names;
+        if (args.length > 0) {
+            names = String.join(", ", args);
+        } else {
+            names = "World";
+        }
+        System.out.println("Hello, " + names);
+    }
+}
