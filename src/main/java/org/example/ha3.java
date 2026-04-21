81eef69bf778436e9caa7c09d4dd0573c7f95323
sbtaher87-max <sbtaher87@gmail.com>
2026-04-21 11:10:07 +0530

    added only ha3

---
+++ src/main/java/org/example/ha3.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class ha3 {
+        public static void main(String[] args) {
+            String praveen = args.length > 0 ? args[0] : "";
+            System.out.println("Hello," + praveen + "!");
+        }
+    }
+
