92f7c9f2a46ee4935aca4f28b4a08eaf57c7119e
sbtaher87-max <sbtaher87@gmail.com>
2026-04-21 11:11:57 +0530

    added only uc4

---
+++ src/main/java/org/example/uc4.java
@@ -0,0 +1,18 @@
+package org.example;
+public class uc4 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            System.out.println("Hello, " + nameBuilder.toString());
+
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
