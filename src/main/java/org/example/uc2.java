978d1712c75f5adb45deabd6cbfb65d1e397d2f9
sbtaher87-max <sbtaher87@gmail.com>
2026-04-21 10:47:36 +0530

    added uc2

--- src/main/java/org/example/uc2.java
+++ src/main/java/org/example/uc2.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class uc2 {
+
+    public static void main(String[] args) {
+
+        String praveen = args[0];
+        System.out.println("Hello, " + praveen + "!");
+    }
 }
+
