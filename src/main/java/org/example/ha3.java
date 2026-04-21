281affdc3afb382f54fe2fb4e5d79ee6fbe267ff
sbtaher87-max <sbtaher87@gmail.com>
2026-04-21 14:13:35 +0530

    Update ha3.java

--- src/main/java/org/example/ha3.java
+++ src/main/java/org/example/ha3.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class ha3 {
         public static void main(String[] args) {
-            String taher = args.length > 0 ? args[0] : "";
+            String taher = args.length > 0 ? args[0] : "taher";
             System.out.println("Hello," +taher+ "!");
         }
     }
