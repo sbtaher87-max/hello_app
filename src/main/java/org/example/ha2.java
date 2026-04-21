b61a4a4c39b17cf3800548adea2f4e68c8f11c88
sbtaher87-max <sbtaher87@gmail.com>
2026-04-21 11:31:41 +0530

    added only ha2

---
+++ src/main/java/org/example/ha2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ha2 {
+
+
+        public static void main(String[] args) {
+
+            String praveen = args[0];
+            System.out.println("Hello, " + praveen + "!");
+        }
+    }
+
--- src/main/java/org/example/uc3.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-public class uc3 {
-    public static void main(String[] args) {
-        String praveen = args.length > 0 ? args[0] : "";
-        System.out.println("Hello," + praveen + "!");
-    }
-}
